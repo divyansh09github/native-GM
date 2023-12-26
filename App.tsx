30b7397c09dd801c9fb79a2fbb4ecb3adc7743fd
sagar1902 <sagar2002pj@gmail.com>
2023-12-26 20:38:24 +0530

    redux setup complete

--- App.tsx
+++ App.tsx
@@ -1,27 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
-import HomeScreen from './components/HomeScreen';
-import ProfileScreen from './components/ProfileScreen';
-import ContactScreen from './components/ContactScreen';
-import DetailsScreen from './components/DetailsScreen';
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import HomeScreen from './src/components/HomeScreen';
+import ProfileScreen from './src/components/ProfileScreen';
+import ContactScreen from './src/components/ContactScreen';
+import DetailsScreen from './src/components/DetailsScreen';
+import {Provider} from 'react-redux';
+import store from './src/store';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name="Home" component={HomeScreen}  />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen}  />
-        <Stack.Screen name="Contact" component={ContactScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  )
-}
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Profile" component={ProfileScreen} />
+          <Stack.Screen name="Details" component={DetailsScreen} />
+          <Stack.Screen name="Contact" component={ContactScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  );
+};
 
-export default App
+export default App;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
